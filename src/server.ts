37ff369a30ee649ee3b0db86dffc4cc097351a29
devfabiorm <fabio.evangelion.guitar@gmail.com>
2021-02-22 19:57:49 -0300

    Setting server and testing routes

--- src/server.ts
+++ src/server.ts
@@ -2,4 +2,13 @@ import express from 'express';
 
 const app = express();
 
+app.get('/', (request, response) => {
+  return response.json({ message: 'Hello World - NLW#4'});
+});
+
+app.post('/', (request, response) => {
+  return response.json({ message: 'Seus dados foram enviados com sucesso!'});
+});
+
+
 app.listen(3333, () => console.log("Server is running"));
