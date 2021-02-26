51bf9e101068c54ff88d504d0c2a88d7ec4cd33a
devfabiorm <fabio.evangelion.guitar@gmail.com>
2021-02-26 03:48:39 -0300

    Creting link to answer and fixing relationships in model

--- src/controllers/SendMailController.ts
+++ src/controllers/SendMailController.ts
@@ -30,6 +30,25 @@ class SendMailController {
       });
     }
 
+    const npsPath = resolve(__dirname, '..', 'views', 'emails', 'npsMail.hbs');
+    const variables = {
+      name: user.name,
+      title: survey.title,
+      description: survey.description,
+      user_id: user.id,
+      link: process.env.URL_MAIL
+    }
+
+    const surveyUserAlreadyExists = await surveysUsersRepository.findOne({
+      where: [{user_id: user.id}, {value: null}],
+      relations: ['user', 'survey']
+    });
+
+    if(surveyUserAlreadyExists) {
+      await SendMailService.execute(email, survey.title, variables, npsPath);
+      return response.json(surveyUserAlreadyExists);
+    }
+
     //Salvar as informações na tabela surverys_users
     const surveyUser = surveysUsersRepository.create({
       user_id: user.id,
@@ -39,13 +58,6 @@ class SendMailController {
     await surveysUsersRepository.save(surveyUser);
 
     //Enviar e-mail para o usuário
-    const npsPath = resolve(__dirname, '..', 'views', 'emails', 'npsMail.hbs');
-    const variables = {
-      name: user.name,
-      title: survey.title,
-      description: survey.description
-    }
-
     await SendMailService.execute(email, survey.title, variables, npsPath);
 
     return response.json(surveyUser);
